ba55f2e391cebe98b8cff4703e4ded057db48ec3
Eurus-J-Zhang <66300568+Eurus-J-Zhang@users.noreply.github.com>
2021-07-13 12:20:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,42 @@
     });
     
 
+    document.querySelector("#btnLeft").addEventListener('click',function(){
+        const random_number = Math.floor(Math.random() * 8)+1;
+        const random_value = "X".concat(random_number.toString());
+        document.getElementById("recommendataion").innerHTML = "Variable review recommendation: ".concat(random_value);
+        const myDataAI = generateDataPairFromX(allData[random_value],allData.Y);
+        const myChartAI = createChart(myDataAI, 'chart-container-AI', random_value, 'Y');
+        console.log('ready :)');
+        document.querySelector('#trybuttontwo').style.visibility="visible";
+        document.querySelector("#trybuttontwo").addEventListener('click',function(){
+            const myData = generateDataPairFromX(allData[random_value], allData.Y);
+            const myDataTwo = generateDataPairFromX(allData[random_value], allData[random_value]);
+            const myChart = createChart(myData, 'chart-container', random_value, 'Y');
+            const myChartTwo = createChart(myDataTwo, 'chart-container-two',random_value, random_value);
+            console.log('ready :)');
+            console.log(random_value);
+        })
+    });
+
+    document.querySelector("#btnRight").addEventListener('click',function(){
+        const random_number = Math.floor(Math.random() * 8)+1;
+        const random_value = "X".concat(random_number.toString());
+        document.getElementById("recommendataion").innerHTML = "Variable review recommendation: ".concat(random_value);
+        const myDataAI = generateDataPairFromX(allData[random_value],allData.Y);
+        const myChartAI = createChart(myDataAI, 'chart-container-AI', random_value, 'Y');
+        console.log('ready :)');
+        document.querySelector('#trybuttontwo').style.visibility="visible";
+        document.querySelector("#trybuttontwo").addEventListener('click',function(){
+            const myData = generateDataPairFromX(allData[random_value], allData.Y);
+            const myDataTwo = generateDataPairFromX(allData[random_value], allData[random_value]);
+            const myChart = createChart(myData, 'chart-container', random_value, 'Y');
+            const myChartTwo = createChart(myDataTwo, 'chart-container-two',random_value, random_value);
+            console.log('ready :)');
+            console.log(random_value);
+        })
+    });
+
 
     let m1,m2,n=[],
         mainObj = {};
@@ -72,68 +108,31 @@ function createChart(data, containerId, xID, yID){
 }
 
 
-
-
-
-
-document.querySelector("#trybuttonone").addEventListener('click',function(){
+document.querySelector('select#variable-one').addEventListener('click',function(){
     const userSelectedX = document.getElementById("variable-one").value;
     const myData = generateDataPairFromX(allData[userSelectedX], allData.Y);
     console.log(allData)
     const myChart = createChart(myData, 'chart-container', userSelectedX, 'Y');
     console.log('ready :)');
-    console.log(userSelectedX)
 })
 
-
-
-document.querySelector("#trybuttonthree").addEventListener('click',function(){
+document.querySelector("select#variable-two").addEventListener('click',function(){
     const userSelectedXleft = document.getElementById("variable-two").value;
     const userSelectedXbottom = document.getElementById("variable-three").value;
     const myDataTwo = generateDataPairFromX(allData[userSelectedXbottom],allData[userSelectedXleft]);
     const myChartTwo = createChart(myDataTwo, 'chart-container-two',userSelectedXbottom, userSelectedXleft);
     console.log('ready :)');
-    console.log(userSelectedXleft);
 })
 
-
-document.querySelector("#GetFB").addEventListener('click',function(){
-    const random_number = Math.floor(Math.random() * 8)+1;
-    const random_value = "X".concat(random_number.toString());
-    document.getElementById("recommendataion").innerHTML = "Variable review recommendation: ".concat(random_value);
-    const myDataAI = generateDataPairFromX(allData[random_value],allData.Y);
-    const myChartAI = createChart(myDataAI, 'chart-container-AI', random_value, 'Y');
+document.querySelector('select#variable-three').addEventListener('click',function(){
+    const userSelectedXleft = document.getElementById("variable-two").value;
+    const userSelectedXbottom = document.getElementById("variable-three").value;
+    const myDataTwo = generateDataPairFromX(allData[userSelectedXbottom],allData[userSelectedXleft]);
+    const myChartTwo = createChart(myDataTwo, 'chart-container-two',userSelectedXbottom, userSelectedXleft);
     console.log('ready :)');
-
-    document.querySelector("#B_Explore").addEventListener('click',function(){
-        const myData = generateDataPairFromX(allData[random_value], allData.Y);
-        const myDataTwo = generateDataPairFromX(allData[random_value], allData[random_value]);
-        const myChart = createChart(myData, 'chart-container', random_value, 'Y');
-        const myChartTwo = createChart(myDataTwo, 'chart-container-two',random_value, random_value);
-        console.log('ready :)');
-        console.log(random_value);
-    })
 })
 
-document.querySelector("#B_Another").addEventListener('click',function(){
-    const random_number = Math.floor(Math.random() * 8)+1;
-    const random_value = "X".concat(random_number.toString());
-    document.getElementById("recommendataion").innerHTML = "Variable review recommendation: ".concat(random_value);
-    const myDataAI = generateDataPairFromX(allData[random_value],allData.Y);
-    const myChartAI = createChart(myDataAI, 'chart-container-AI', random_value, 'Y');
-    console.log('ready :)');
-
-    document.querySelector("#B_Explore").addEventListener('click',function(){
-        const myData = generateDataPairFromX(allData[random_value], allData.Y);
-        const myDataTwo = generateDataPairFromX(allData[random_value], allData[random_value]);
-        const myChart = createChart(myData, 'chart-container', random_value, 'Y');
-        const myChartTwo = createChart(myDataTwo, 'chart-container-two',random_value, random_value);
-        console.log('ready :)');
-        console.log(random_value);
-    })
-})
-
-
+document.querySelector('#trybuttontwo').style.visibility="hidden";
 
 fetch("./data.json")
     .then(response => response.json())
@@ -143,7 +142,7 @@ fetch("./data.json")
         const myChart = createChart(myData, 'chart-container', 'X1', 'Y');
         const myDataTwo = generateDataPairFromX(allData.X1, allData.X1);
         const myChartTwo = createChart(myDataTwo, 'chart-container-two','X1', 'X1');
-        const myDataAI = generateDataPairFromX(allData.X1,allData.Y);
-        const myChartAI = createChart(myDataAI, 'chart-container-AI', 'X1', 'Y');
+        // const myDataAI = generateDataPairFromX(allData.X1,allData.Y);
+        // const myChartAI = createChart(myDataAI, 'chart-container-AI', 'X1', 'Y');
     });
 
